182f5ad76c195416133df2bfc4b67f8a5d066ccd
ChillerDragon <ChillerDragon@gmail.com>
2023-09-17 20:08:31 +0200

    Add left and right endpoints

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ bundle
 ruby client.rb
 
 curl -X POST http://localhost:4567/connect -F host=localhost -F port=8303
+curl -X POST http://localhost:4567/left -F ticks=200
+curl -X POST http://localhost:4567/right -F ticks=600
 curl -X POST http://localhost:4567/disconnect
 curl -X POST http://localhost:4567/messages -F message='hello world'
 curl http://localhost:4567/messages
--- client.rb
+++ client.rb
@@ -6,6 +6,9 @@ at_exit { client.disconnect }
 require 'sinatra'
 
 messages = []
+inp_left_ticks = 0
+inp_right_ticks = 0
+inp_jump_ticks = 0
 client_connected = false
 client.set_startinfo(
   name: 'zillyhuhn.com',
@@ -45,6 +48,45 @@ client.on_chat do |_, msg|
   messages << {author:, message: msg.message}
 end
 
+client.on_tick do
+  send_inp = false
+  dir = 0
+  jmp = 0
+  inp_left_ticks -= 1
+  inp_right_ticks -= 1
+  inp_jump_ticks -= 1
+  if inp_left_ticks == -1 || inp_right_ticks == -1 || inp_jump_ticks == -1
+    # send that we stopped moving
+    send_inp = true
+  end
+  if inp_left_ticks.positive?
+    send_inp = true
+    dir = -1
+  end
+  if inp_right_ticks.positive?
+    send_inp = true
+    dir = 1
+  end
+  if inp_jump_ticks.positive?
+    send_inp = true
+    jmp = 1
+  end
+  # puts send_inp
+  # next unless send_inp
+
+  client.send_input(
+        direction: dir,
+        target_x: 10,
+        target_y: 10,
+        jump: jmp,
+        fire: 0,
+        hook: 0,
+        player_flags: 0,
+        wanted_weapon: 0,
+        next_weapon: 0,
+        prev_weapon: 0)
+end
+
 get '/' do
   @hostname = ENV['HOSTNAME'] || 'http://localhost:4567'
   erb :index
@@ -68,6 +110,28 @@ post '/connect' do
     return 'Already connected'
   end
   client.connect(host, port.to_i, detach: true)
+  'OK'
+end
+
+post '/jump' do
+  ticks = params[:ticks] || 0
+  ticks = ticks.to_i
+  inp_jump_ticks = ticks
+  'OK'
+
+end
+
+post '/left' do
+  ticks = params[:ticks] || 0
+  ticks = ticks.to_i
+  inp_left_ticks = ticks
+  'OK'
+end
+
+post '/right' do
+  ticks = params[:ticks] || 0
+  ticks = ticks.to_i
+  inp_right_ticks = ticks
   'OK'
 end
 
